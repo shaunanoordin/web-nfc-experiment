f8a4ffd6bf40f8f5d25d76a0ede34fe53bfc4950
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-05-06 15:16:06 +0100

    Add first action button

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ class App {
   constructor () {
     this.html = {
       status: document.getElementById('status'),
-      
+      data: document.getElementById('data'),
+      scanButton: document.getElementById('scan-button'),
     };
+    
+    this.html.scanButton.onclick = this.doScan.bind(this);
     
     if (this.isNfcCompatible()) {
       this.updateStatus('Your device & browser supports NFC.', 'ok');
@@ -16,15 +19,34 @@ class App {
     
   }
   
+  async doScan () {
+    this.updateData(`Beep beep ${(new Date())}`)
+  }
+  
   updateStatus (text, type) {
     this.html.status.textContent = text;
     this.html.status.className = `status-${type}`;
   }
   
+  updateData (text) {
+    this.html.data.textContent = text;
+  }
+  
   isNfcCompatible () {
-    return !!('NDEFReader' in window)
+    return !!(window.NDEFReader);
   }
 }
+
+/*function debugObject (obj, depth = 0) {
+  let output = '';
+  Object.keys(obj).forEach((key) => {
+    
+    
+    output += '.'.repeat(depth)
+      + key + ':' + obj
+  })
+
+}*/
 
 
 window.onload = function init () {
